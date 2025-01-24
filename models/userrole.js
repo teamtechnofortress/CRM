27f106b24f4e421409ce8254343355b82130b445
Zulnurain <meet.zulnurain@gmail.com>
2025-01-24 16:43:34 +0500

    model

--- models/userrole.js
+++ models/userrole.js
@@ -3,10 +3,8 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const roleSchema = new Schema({
-
-    role: { type: String, required: true },
-    permissions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Permission" }],
- 
-}, { timestamps: true });   
+  role: { type: String, required: true },
+  permissions: [{ type: mongoose.Schema.Types.ObjectId, ref: "permissions" }], // Ensure the 'ref' matches the model name
+}, { timestamps: true });
 
 export default mongoose.models.Role || mongoose.model("Role", roleSchema);
